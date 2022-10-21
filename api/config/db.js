3dbc15dcfc7b97ca26ffeca35864ce9557748f9e
yamolatix <yamilaortiztoscano@gmail.com>
2022-10-21 12:08:43 -0300

    correcciones

--- api/config/db.js
+++ api/config/db.js
@@ -1,24 +1,35 @@
 //Importo la función utilizada para producción de mysql
 const { createPool } = require('mysql');
 
+// CHEQUEAR EL .ENV!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+//Requiero el .env
+/* require('dotenv').config(); */
+
 // Creo el objeto de conexion y lo guardo en una constante
+/* const pool = createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+}); */
+
 const pool = createPool({
-    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
-    user: 'bsale_test',
-    password: 'bsale_test',
-    database: 'bsale_test',
+    host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+    user: "bsale_test",
+    password: "bsale_test",
+    database: "bsale_test",
 });
 
-function handleConnection() {
+/* function handleConnection() {
     pool.getConnection(function (err, connection) {
         if (err) {
-            console.error("mysql getConnection err", err);
+            console.error('mysql getConnection err', err);
             return;
         }
         connection.ping();
         connection.release();
     });
 }
-setInterval(handleConnection, 5000);
+setInterval(handleConnection, 5000); */
 
 module.exports = pool;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 // Express para las rutas
 const express = require('express');
 
+//Requiero el .env
+require("dotenv").config();
+
 // Middelwares
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -15,26 +18,36 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(volleyball)
-app.use(
-    cors({
-        header: 'Access-Control-Allow-Origin: *',
-        methods: 'PUT, GET, POST, DELETE, OPTIONS',
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    header: 'Origin, X-Requested-With, Content-Type, Accept'
+}))
 
 // Routes
 app.use('/api', routes);
 
 // El Middleware para manejo de errores posee un parámetro extra, en este caso lo llamo err
 // Este último Middleware detecta los errores y los coloca en dicho parámetro
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Some custom error!', err.message);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    const status = err.status || 500;
+    res.status(status);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
 // Conecto servidor
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
